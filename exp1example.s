1ac14d8b0d9d88f42d5ae731ddac13a55f5edcf4
maha_mali <99613493+maha123m@users.noreply.github.com>
2022-09-01 16:39:57 +0300

    Add files via upload

---
+++ exp1example.s
@@ -0,0 +1,36 @@
+;The semicolon is used to lead an inline documentation.
+;This is the first ARM Assembly language program you see in the lab.
+;This program skeleton was from Dave Duguid and Trevor Douglas in summer 2013.
+;When you write your program, you could have your info at the top document block.
+;For Example: Your Name, Student Number, what the program is for, and what it does 
+
+;;; Directives
+		PRESERVE8
+		THUMB 
+; Vector Table Mapped to Address 0 at Reset
+; Linker requires __Vectors to be exported
+		AREA RESET, DATA, READONLY
+		EXPORT __Vectors
+__Vectors 
+	DCD 0x20001000 ; stack pointer value when stack is empty
+;The processor uses a full descending stack. 
+;This means the stack pointer holds the address of the last 
+;stacked item in memory. When the processor pushes a new item 
+;onto the stack, it decrements the stack pointer and then 
+;writes the item to the new memory location.
+		DCD Reset_Handler ; reset vector
+ 
+		ALIGN
+; The program
+; Linker requires Reset_Handler
+
+			AREA MYCODE, CODE, READONLY
+		ENTRY
+		EXPORT Reset_Handler
+Reset_Handler
+;;;;;;;;;;User Code Starts from the next line;;;;;;;;;;;;
+		MOV R0, #12
+STOP 
+		ADD R0, R0, #4
+				B STOP
+			END ;End of the program
